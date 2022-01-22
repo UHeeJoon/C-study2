b00002beee271cc9289508e930bdfdddd9acd24d
UHeeJoon <yhj3855@naver.com>
2022-01-23 00:01:22 +0900

    코테 연습

---
+++ BaekJoon/2529번.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int k;
+char sign[10], num[11];
+bool check[10];
+int main() {
+	cin >> k;
+	int tmp = 0;
+	for (int i = 0; i < k; i++) {
+		cin >> sign[i];
+		if (sign[i] == '<')
+			check[i] == 1;
+	}
+	for (int i = 0; i < k; i++) {
+		if (check[i]) {
+			
+		}
+	}
+}
+
+//, num[10] = { '0','1','2','3','4','5','6','7','8','9' }
