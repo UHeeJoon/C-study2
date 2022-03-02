c43bfba88ad69bb5346e2fe6fcdae3e617607903
UHeeJoon <yhj3855@naver.com>
2022-03-02 18:26:46 +0900

    코테 연습

--- BaekJoon/질의응답.cpp
+++ BaekJoon/질의응답.cpp
@@ -1,25 +1,43 @@
 #pragma warning (disable :4996)
 #include<iostream>
 #include<algorithm>
-#define MAX 9
+#include<string>
+#include<vector>
+#include<queue>
+#include<cstring>
+#include<cstdio>
+#include<tuple>
+#include<cmath>
 using namespace std;
-int n, m;
-int arr[MAX] = { 0, };
-bool visited[MAX] = { 0, };
-void dfs(int num, int cnt) {
-    if (cnt == m) {
-        for (int i = 0; i < m; i++)
-            cout << arr[i] << " ";
-        cout << "\n";
-        return;
-    }
-    for (int i = num; i <= n; i++) {
-        arr[cnt] = i;
-        dfs(i, cnt + 1);
-    }
+int	rope[100001], n, k;
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> rope[i];
+	sort(rope, rope + n);
+	for (int i = 0; i < n; i++) 
+		k = max(rope[i] * (n - i), k);
+	cout << k << '\n';
+	return 0;
 }
+
+/*
+#pragma warning (disable :4996)
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<vector>
+#include<queue>
+#include<cstring>
+#include<cstdio>
+#include<tuple>
+#include<cmath>
+using namespace std;
+
 int main() {
     ios_base::sync_with_stdio(false); cout.tie(NULL);
-    cin >> n >> m;
-    dfs(1, 0);
+
+
 }
+
+*/
