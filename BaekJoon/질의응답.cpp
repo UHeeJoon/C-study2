55c7b3b65c9e556276aa852098f1e79cd5525c1d
UHeeJoon <yhj3855@naver.com>
2022-03-27 01:42:13 +0900

    코테 연습

--- BaekJoon/질의응답.cpp
+++ BaekJoon/질의응답.cpp
@@ -3,13 +3,14 @@
 using namespace std;
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int t = 3;
-    while (t--) {
-        int h, m, s, h2, m2, s2; cin >> h >> m >> s >> h2 >> m2 >> s2;
-        h = h2 - h, m = m2 - m, s = s2 - s;
-        if (s < 0) s = 60 + s, m = m - 1;
-        if (m < 0) m = 60 + m, h = h - 1;
-        cout << h << ' ' << m << ' ' << s << '\n';
+    int n; cin >> n;
+    if (n <= 5) cout << n << '\n';
+    else
+    {
+        if ((n - 5) / 4 % 2 == 0)
+            cout << 5 - (n - 5) % 4 << '\n';
+        else
+            cout << 1 + (n - 5) % 4 << '\n';
     }
     return 0;
 }
