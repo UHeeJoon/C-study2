49132cd8e76f46052e26de9a2b649200422029e2
UHeeJoon <yhj3855@naver.com>
2022-03-26 23:08:52 +0900

    코테 연습

--- BaekJoon/질의응답.cpp
+++ BaekJoon/질의응답.cpp
@@ -1,24 +1,15 @@
 #pragma warning(disable :4996)
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-int dp[1000000];
-int Plus(int n) {
-	dp[1] = 1;
-	dp[2] = 2;
-	dp[3] = 4;
-	for (int i = 4; i <= n; i++) {
-		dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
-	}
-	return dp[n];
-}
 int main() {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	int t, n;
-	cin >> t;
-
-	for (int i = 0; i < t; i++) {
-		cin >> n;
-		cout << Plus(n) << '\n';
-	}
-	return 0;
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int t = 3;
+    while (t--) {
+        int h, m, s, h2, m2, s2; cin >> h >> m >> s >> h2 >> m2 >> s2;
+        h = h2 - h, m = m2 - m, s = s2 - s;
+        if (s < 0) s = 60 + s, m = m - 1;
+        if (m < 0) m = 60 + m, h = h - 1;
+        cout << h << ' ' << m << ' ' << s << '\n';
+    }
+    return 0;
 }
