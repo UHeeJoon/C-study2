5091013026bd8ca0c425f539a0dd235acf113a34
희준 유 <yhj3855@naver.com>
2022-07-04 23:49:30 +0900

    코테 연습

--- BaekJoon/질의응답.cpp
+++ BaekJoon/질의응답.cpp
@@ -3,13 +3,50 @@
 using namespace std;
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int n, a = 0, b;
-    cin >> n;
-    n -= 1;
     while (1) {
-        a++;
-        if (a * a + a == n)break;
+        string s; getline(cin, s);
+        if (s == "#")break;
+        int cons = 0;
+        for (int i = 0; i < (int)s.length(); i++)
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u'
+                || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
+                cons++;
+        cout << cons << '\n';
     }
-    cout << a << '\n';
 	return 0;
 }
+
+
+/*
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+string string_add(string a, string b) {
+    int sum = 0;
+    string ret;
+    while (a.size() || b.size() || sum) {
+        if (a.size()) {
+            sum += a.back() - '0';
+            a.pop_back();
+        }
+        if (b.size()) {
+            sum += b.back() - '0';
+            b.pop_back();
+        }
+        ret += (sum % 10) + '0';
+        sum /= 10;
+    }
+    reverse(ret.begin(), ret.end());
+    return ret;
+}
+string a, b;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> a >> b;
+    cout << string_add(a, b) << '\n';
+    return 0;
+}
+*/
