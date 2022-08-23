fe8e33c6f378d2bfc090b55972e098336a0c70e6
희준 유 <yhj3855@naver.com>
2022-08-23 21:23:05 +0900

    코테 연

--- BaekJoon/질의응답.cpp
+++ BaekJoon/질의응답.cpp
@@ -1,96 +1,129 @@
 #pragma warning(disable : 4996)
 #include<bits/stdc++.h>
 using namespace std;
-#define MAX 8
+#define MAX 17
 #define INF 2147483647
-int boomCnt = 0, cnt = 1;
-char puyoPuyo[MAX + MAX][MAX];
-bool visited[MAX + MAX][MAX];
-int dy[] = { 1,0,-1,0 };
-int dx[] = { 0,1,0,-1 };
-void check(int y, int x) {
-	visited[y][x] = true;
-	for (int i = 0; i < 4; i++) {
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-		if (ny < 0 || ny >= 12 || nx < 0 || nx >= 6|| visited[ny][nx] || puyoPuyo[y][x] != puyoPuyo[ny][nx] || puyoPuyo[ny][nx] == '.') continue;
-		visited[ny][nx] = true;
-		check(ny, nx);
-		cnt++;
-	}
-	return;
+bool board[MAX][MAX];
+bool tmpBoard[MAX][MAX];
+int tmp[4];
+int n, m, d;
+int _max = 0;
+bool check(int aY, int aX, int eY, int eX) {
+	return abs(aY - eY) + abs(aX - eX) <= d;
 }
-void boom(int y, int x, char shape) {
-	queue<pair<int, int>> q;
-	q.push({ y, x });
-	while (!q.empty()) {
-		int y = q.front().first;
-		int x = q.front().second;
-		q.pop();
-		puyoPuyo[y][x] = '.';
-		for (int i = 0; i < 4; i++) {
-			int ny = y + dy[i];
-			int nx = x + dx[i];
-			if (ny < 0 || ny >= 12 || nx < 0 || nx >= 6 || puyoPuyo[ny][nx] != shape) continue;
-			q.push({ ny, nx });
+bool allEnemyKill() {
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= m; j++)
+			if (tmpBoard[i][j])return true;
+	return false;
+}
+void moveBoard() {
+	for (int i = n; i > 0; i--) {
+		for (int j = 1; j <= m; j++) {
+			tmpBoard[i][j] = tmpBoard[i - 1][j];
+			tmpBoard[i - 1][j] = false;
 		}
 	}
 	return;
 }
-bool isBoom() {
-	memset(visited, 0, sizeof(visited));
-	bool flag = false;
-	for (int i = 0; i < 12; i++) {
-		for (int j = 0; j < 6; j++) {
-			if (puyoPuyo[i][j] != '.' && !visited[i][j]) {
-				check(i, j);
-				if (cnt >= 4) {
-					boom(i, j, puyoPuyo[i][j]);
-					flag = true;
-				}
-				cnt = 1;
-			}
-		}
-	}
-	return flag;
+void boardCopy() {
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= m; j++)
+			tmpBoard[i][j] = board[i][j];
+	return;
 }
-void gravity() {
-	for (int i = 0; i < 6; i++) {
-		int idx = 11;
-		while(idx--) {
-			if (puyoPuyo[idx][i] != '.') {
-				int tmp = idx + 1;
-				if (puyoPuyo[tmp][i] == '.') {
-					while (tmp <= 11 && puyoPuyo[tmp][i] == '.') {
-						tmp++;
+void killEnemy() {
+	int cnt = 0;
+	boardCopy();
+	while (allEnemyKill()) {
+		for (int i = 1; i <= 3; i++) {
+			int archerY = n + 1;
+			int archerX = tmp[i];
+			bool kill = false;
+			for (int j = 0; j < d; j++) {
+				int enemyY = n - j;
+				int enemyX = m;
+				int k = 0;
+				while(enemyX - k >=1 || enemyX + k <= m) {
+					if (enemyX - k >= 1) {
+						if (tmpBoard[enemyY][enemyX - k] && check(archerY, archerX, enemyY, enemyX - k)) {
+							tmpBoard[enemyY][enemyX - k] = false;
+							kill = true;
+							cnt++;
+							break;
+						}
 					}
-					puyoPuyo[tmp - 1][i] = puyoPuyo[idx][i];
-					puyoPuyo[idx][i] = '.';
+					if (enemyX + k <= m) {
+						if (tmpBoard[enemyY][enemyX + k] && check(archerY, archerX, enemyY, enemyX + k)) {
+							tmpBoard[enemyY][enemyX + k] = false;
+							kill = true;
+							cnt++;
+							break;
+						}
+					}
+					k++;
 				}
+				if (kill)break;
 			}
 		}
+		moveBoard();
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= m; j++)
+				cout << tmpBoard[i][j];
+			cout << '\n';
+		}
+		cout << '\n';
+	}
+
+	//cout << cnt << '\n';
+	_max = max(_max, cnt);
+	return;
+}
+void dfs(int start,int idx, int cnt) {
+	if (idx == cnt + 1) {
+		//for (int i = 1; i <= cnt; i++) {
+		//	cout << tmp[i] << ' ';
+		//}
+		//cout << '\n';
+		killEnemy();
+		return;
+	}
+	for (int i = start; i <= m; i++) {
+		tmp[idx] = i;
+		dfs(i + 1, idx + 1, cnt);
 	}
 	return;
 }
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	for (int i = 0; i < 12; i++) {
-		for (int j = 0; j < 6; j++) {
-			cin >> puyoPuyo[i][j];
+	cin >> n >> m >> d;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			cin >> board[i][j];
 		}
 	}
-	int a = 6;
-	while (isBoom()) {
-		gravity();
-		boomCnt++;
-		//for (int i = 0; i < 12; i++) {
-		//	for (int j = 0; j < 6; j++) {
-		//		cout << puyoPuyo[i][j];
-		//	}
-		//	cout << '\n';
-		//}
-		//cout << '\n';
-	}
-	cout << boomCnt << '\n';
+	dfs(1,1, 3);
+	cout << _max << '\n';
 	return 0;
 }
+
+/*
+
+15 15 10
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+
+*/
