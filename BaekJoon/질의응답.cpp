8d28902cc88d9edd3bb7eab2c96931bd30a1e473
희준 유 <yhj3855@naver.com>
2022-08-30 21:08:33 +0900

    코테 연

--- BaekJoon/질의응답.cpp
+++ BaekJoon/질의응답.cpp
@@ -1,18 +1,16 @@
 #pragma warning(disable : 4996)
 #include<bits/stdc++.h>
 using namespace std;
-#define MAX 1000004
+#define MAX 110
 #define INF 2147483647
-int n, lis[MAX], len, num;
+
 int main() {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> num;
-        auto lowerPos = lower_bound(lis, lis + len, num);
-        if (*lowerPos == 0) len++;
-        *lowerPos = num;
-    }
-    cout << len << '\n';
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	for (int i = 1;;i++) {
+		int l, p, v; cin >> l >> p >> v;
+		if (!l && !p && !v) break;
+		cout << "Case " << i << ": " << l * (v / p) + ((v % p) > l ? l : v % p) << '\n';;
+	}
 	return 0;
 }
+
