6ed9856619c1f12bf6c7147505d07a1c6d980c0c
희준 유 <yhj3855@naver.com>
2022-09-16 00:28:27 +0900

    코테 연

--- BaekJoon/질의응답.cpp
+++ BaekJoon/질의응답.cpp
@@ -5,24 +5,25 @@ using namespace std;
 //#define INF 2'100'000'000
 //#define MOD 1'000'000'007
 //typedef long long ll;
+int cnt = 0;
+int recursion(string &s, int l, int r) {
+    cnt++;
+    if (l >= r) return 1;
+    else if (s[l] != s[r]) return 0;
+    else return recursion(s, l + 1, r - 1);
+}
 
+int isPalindrome(string &s) {
+    return recursion(s, 0, s.length() - 1);
+}
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	int n; cin >> n;
-	int _max = 0, cnt = 0, h = 0;
-	while (n--) {
-		int a;
-		cin >> a;
-		if (h < a) {
-			_max = max(cnt, _max);
-			h = a;
-			cnt = 0;
-		}
-		else {
-			cnt++;
-		}
-	}
-	cout << max(_max, cnt ) << '\n';
+    int tc; cin >> tc;
+    while (tc--) {
+        string s; cin >> s;
+        cout << isPalindrome(s) << ' ' << cnt  << '\n';
+        cnt = 0;
+    }
 	return 0;
 }
 
