2dcb29ad726b12857b13579eba52e51c06cc7c84
희준 유 <yhj3855@naver.com>
2022-10-02 17:12:39 +0900

    코테 연

--- BaekJoon/12904번.cpp
+++ BaekJoon/12904번.cpp
@@ -1,10 +1,5 @@
-#pragma warning(disable : 4996)
 #include<bits/stdc++.h>
 using namespace std;
-//#define MAX 4010
-//#define INF 2'100'000'000
-//#define MOD 1000
-//typedef long long ll;
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	string s, t; cin >> s >> t;
@@ -16,7 +11,7 @@ int main() {
 		if (t[t.length() - 1] == 'A') {
 			t.pop_back();
 		}
-		else if(t[t.length() - 1] == 'B') {
+		else if (t[t.length() - 1] == 'B') {
 			t.pop_back();
 			reverse(t.begin(), t.end());
 		}
