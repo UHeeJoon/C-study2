2d671a2c348600c20865368d9541ad557111bafd
희준 유 <yhj3855@naver.com>
2022-11-25 19:19:06 +0900

    코테 연

--- BaekJoon/질의응답.cpp
+++ BaekJoon/질의응답.cpp
@@ -5,20 +5,22 @@ using namespace std;
 //#define INF 2'100'000'000
 //#define MOD 1000
 //typedef unsigned long long ull;
-typedef long long ll;
+//typedef long long ll;
+
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	ll n, k; cin >> n >> k;
-	vector<ll>arr(n);
-	for (int i = 0; i < n; i++)
-		cin >> arr[i];
-
-	ll sum = 0;
-	sort(arr.begin(), arr.end());
-	for (ll i = 1; i < n; i++) {
-		ll tmp = arr[i] * min(i, k);
-		sum += tmp;
+	int tc; cin >> tc;
+	while (tc--) {
+		int n; cin >> n;
+		vector<int> v(n + 1);
+		for (int i = 1; i <= n; i++)cin >> v[i];
+		int cnt(1);
+		int tmp = v[1];
+		while (cnt <= n && tmp != n) {
+			tmp = v[tmp];
+			cnt++;
+		}
+		cout << (cnt > n ? 0 : cnt) << '\n';
 	}
-	cout << sum;
 	return 0;			  
 }
