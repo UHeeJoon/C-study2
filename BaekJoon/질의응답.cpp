1eaa1e38dcb58db94afcd29767877df946a0b78c
YooHeeJoon <yhjyhj3855@gmail.com>
2023-01-05 20:14:27 +0900

    코테 연습

--- BaekJoon/질의응답.cpp
+++ BaekJoon/질의응답.cpp
@@ -5,16 +5,18 @@ using namespace std;
 //#define INF 2'100'000'000
 //#define MOD 1000
 //typedef unsigned long long ull;
-//typedef long long ll;
-vector<int> v;
+typedef long long ll;
+ll fibo[80];
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	int n; cin >> n;
-	while (n--) {
-		int num; cin >> num;
-		v.push_back(num);
+	fibo[0] = fibo[1] = 1L; fibo[2] = 2L; fibo[3] = 4L;
+	for (int i = 4; i <= 67; i++) {
+		fibo[i] = fibo[i - 1] + fibo[i - 2] + fibo[i - 3] + fibo[i - 4];
 	}
-	sort(v.begin(), v.end(), greater<>());
-	for (int a : v) cout << a << '\n';
+	int tc; cin >> tc;
+	while (tc--) {
+		int n; cin >> n;
+		cout << fibo[n] << '\n';
+	}
 	return 0;
 }
