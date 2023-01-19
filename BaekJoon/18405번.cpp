8c1e23f9d310565fffcb75595992384b2d58dfc3
YooHeeJoon <yhjyhj3855@gmail.com>
2023-01-20 00:50:21 +0900

    경쟁적 전염(BFS) 입력 부분 모듈화 및 가독성 업글

--- BaekJoon/18405번.cpp
+++ BaekJoon/18405번.cpp
@@ -3,14 +3,16 @@
 using namespace std;
 
 #define FAST_IO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
-#define VIRUS_ROW(virus) get<1>(virus).first
-#define VIRUS_COLUMN(virus) get<1>(virus).second
-#define TIME(virus) get<2>(virus)
-#define TYPE(virus) get<0>(virus)
 
-
-typedef tuple<int, pair<int, int>, int> type_location_time;
+typedef pair<int, int> location;
+typedef tuple<int, location, int> type_location_time;
 typedef vector<vector<int>> square_vector;
+
+#define ROW first
+#define COLUMN second
+#define TYPE(type_location_time) get<0>(type_location_time)
+#define LOCATION(type_location_time) {get<1>(type_location_time).ROW, get<1>(type_location_time).COLUMN}
+#define TIME(type_location_time) get<2>(type_location_time)
 
 constexpr int row_to_move[] = { 1,0,-1,0 };
 constexpr int column_to_move[] = { 0,1,0,-1 };
@@ -20,20 +22,20 @@ bool is_exist(const int virus)
 	return virus > 0;
 }
 
-bool check_out_of_size(const int size_test_tube, const int row_of_virus, const int column_of_virus)
+bool check_out_of_size(const int size_test_tube, const int virus_row, const int virus_column)
 {
-	if (row_of_virus < 0 || row_of_virus >= size_test_tube || column_of_virus < 0 || column_of_virus >= size_test_tube) return true;
+	if (virus_row < 0 || virus_row >= size_test_tube || virus_column < 0 || virus_column >= size_test_tube) return true;
 	return false;
 }
 
-void infect_virus(square_vector& test_tube, const int size_test_tube, queue<type_location_time>& viruses, const int time_to_check)
+void infect_virus(square_vector& test_tube, const int test_tube_size, queue<type_location_time>& viruses, const int time_to_check)
 {
 	while (!viruses.empty())
 	{
 		const type_location_time virus{ viruses.front() };
 		viruses.pop();
 
-		const int type_of_virus = TYPE(virus);
+		const int virus_type = TYPE(virus);
 		const int time_current = TIME(virus) + 1;
 
 		// end condition
@@ -41,30 +43,25 @@ void infect_virus(square_vector& test_tube, const int size_test_tube, queue<type
 
 		for (int move = 0; move < 4; move++)
 		{
-			const int row_of_virus = VIRUS_ROW(virus) + row_to_move[move];
-			const int column_of_virus = VIRUS_COLUMN(virus) + column_to_move[move];
+			const location virus_location = LOCATION(virus);
+			const int virus_row = virus_location.ROW + row_to_move[move];
+			const int virus_column = virus_location.COLUMN + column_to_move[move];
 
-			if (check_out_of_size(size_test_tube, row_of_virus, column_of_virus) || is_exist(test_tube[row_of_virus][column_of_virus]))continue;
+			if (check_out_of_size(test_tube_size, virus_row, virus_column) || is_exist(test_tube[virus_row][virus_column]))continue;
 
-			test_tube[row_of_virus][column_of_virus] = type_of_virus;
-			viruses.push({ type_of_virus, {row_of_virus, column_of_virus}, time_current });
+			test_tube[virus_row][virus_column] = virus_type;
+			viruses.push({ virus_type, {virus_row, virus_column}, time_current });
 		}
 	}
 }
-void solve()
+
+void input_variables(queue<type_location_time>& viruses, square_vector& test_tube, const int test_tube_size)
 {
-	FAST_IO;
-
-	int size_test_tube, max_virus_type;
-	cin >> size_test_tube >> max_virus_type;
-
-	queue<type_location_time> viruses;
 	vector<type_location_time> tmp_sort_by_virus_type;
-	square_vector test_tube(size_test_tube, vector<int>(size_test_tube));
 
-	for (int row = 0; row < size_test_tube; row++)
+	for (int row = 0; row < test_tube_size; row++)
 	{
-		for (int col = 0; col < size_test_tube; col++)
+		for (int col = 0; col < test_tube_size; col++)
 		{
 			int virus_type;
 			cin >> virus_type;
@@ -81,14 +78,24 @@ void solve()
 	{
 		viruses.push(sort_by_virus_type_element);
 	}
+}
 
-	// free memory
-	vector<type_location_time>().swap(tmp_sort_by_virus_type);
+void solve()
+{
+	FAST_IO;
+
+	int test_tube_size, max_virus_type;
+	cin >> test_tube_size >> max_virus_type;
+
+	queue<type_location_time> viruses;
+	square_vector test_tube(test_tube_size, vector<int>(test_tube_size));
+
+	input_variables(viruses, test_tube, test_tube_size);
 
 	int time_to_check, row_to_check, column_to_check;
 	cin >> time_to_check >> row_to_check >> column_to_check;
 
-	infect_virus(test_tube, size_test_tube, viruses, time_to_check);
+	infect_virus(test_tube, test_tube_size, viruses, time_to_check);
 
 	cout << test_tube[row_to_check - 1][column_to_check - 1] << '\n';
 }
