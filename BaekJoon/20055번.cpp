4b9d14a474720e8378226d8596f3c7ed093e6336
YooHeeJoon <yhjyhj3855@gmail.com>
2023-01-19 22:00:42 +0900

    컨베이어 벨트 위의 로봇 (구현)

--- BaekJoon/20055번.cpp
+++ BaekJoon/20055번.cpp
@@ -1,8 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define DURABILITY first
+#define IS_EXIST second
 void rotate_robot(vector<pair<int, bool>>& belt, const int belt_size)
 {
-	belt[belt_size / 2 - 1].second = false;
+	belt[belt_size / 2 - 1].IS_EXIST = false;
 	for (int i = 0; i < belt_size; i++)
 	{
 		swap(belt[0], belt[i]);
@@ -10,33 +13,33 @@ void rotate_robot(vector<pair<int, bool>>& belt, const int belt_size)
 }
 void move_robot(vector<pair<int, bool>>& belt, const int half_of_belt_size, int& zero_count)
 {
-	belt[half_of_belt_size].second = false;
+	belt[half_of_belt_size].IS_EXIST = false;
 	for (int i = half_of_belt_size - 1; i >= 0; i--)
 	{
-		if (belt[i].second && !belt[i + 1].second && belt[i + 1].first)
+		if (belt[i].IS_EXIST && !belt[i + 1].IS_EXIST && belt[i + 1].DURABILITY)
 		{
-			belt[i + 1].first -= 1;
-			if (!belt[i + 1].first) { zero_count += 1; }
-			belt[i + 1].second = true;
-			belt[i].second = false;
+			belt[i + 1].DURABILITY -= 1;
+			if (!belt[i + 1].DURABILITY) { zero_count += 1; }
+			belt[i + 1].IS_EXIST = true;
+			belt[i].IS_EXIST = false;
 		}
 	}
-	if (belt[0].first && !belt[0].second)
+	if (belt[0].DURABILITY && !belt[0].IS_EXIST)
 	{
-		belt[0].first -= 1;
-		if (!belt[0].first) { zero_count += 1; }
-		belt[0].second = true;
+		belt[0].DURABILITY -= 1;
+		if (!belt[0].DURABILITY) { zero_count += 1; }
+		belt[0].IS_EXIST = true;
 	}
 }
 int main() {
-	ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+	FAST_IO;
 	int n, k;
 	cin >> n >> k;
 	const int belt_size = 2 * n;
 	vector<pair<int, bool>>belt(belt_size);
 	for (int i = 0; i < belt_size; i++)
 	{
-		cin >> belt[i].first;
+		cin >> belt[i].DURABILITY;
 	}
 	int robot_move_count = 0;
 	int zero_count = 0;
