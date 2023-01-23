553bdb447fe7b90208c8db87c7717a918c2d394a
YooHeeJoon <yhjyhj3855@gmail.com>
2023-01-23 17:40:22 +0900

    경쟁적 전염(BFS) 최후 수정 + 다른 사람 풀이

--- BaekJoon/18405번.cpp
+++ BaekJoon/18405번.cpp
@@ -22,9 +22,9 @@ bool is_exist(const int virus)
 	return virus > 0;
 }
 
-bool check_out_of_size(const int size_test_tube, const int virus_row, const int virus_column)
+bool check_out_of_size(const int test_tube_size, const int virus_row, const int virus_column)
 {
-	if (virus_row < 0 || virus_row >= size_test_tube || virus_column < 0 || virus_column >= size_test_tube) return true;
+	if (virus_row < 0 || virus_row >= test_tube_size || virus_column < 0 || virus_column >= test_tube_size) return true;
 	return false;
 }
 
@@ -57,7 +57,7 @@ void infect_virus(square_vector& test_tube, const int test_tube_size, queue<type
 
 void input_variables(queue<type_location_time>& viruses, square_vector& test_tube, const int test_tube_size)
 {
-	vector<type_location_time> tmp_sort_by_virus_type;
+	vector<type_location_time> sort_by_virus_type;
 
 	for (int row = 0; row < test_tube_size; row++)
 	{
@@ -68,13 +68,13 @@ void input_variables(queue<type_location_time>& viruses, square_vector& test_tub
 			test_tube[row][col] = virus_type;
 			if (is_exist(virus_type))
 			{
-				tmp_sort_by_virus_type.push_back({ virus_type,{row, col},0 });
+				sort_by_virus_type.push_back({ virus_type,{row, col},0 });
 			}
 		}
 	}
 
-	sort(tmp_sort_by_virus_type.begin(), tmp_sort_by_virus_type.end());
-	for (type_location_time& sort_by_virus_type_element : tmp_sort_by_virus_type)
+	sort(sort_by_virus_type.begin(), sort_by_virus_type.end());
+	for (type_location_time& sort_by_virus_type_element : sort_by_virus_type)
 	{
 		viruses.push(sort_by_virus_type_element);
 	}
@@ -106,3 +106,68 @@ int main() {
 
 	return 0;
 }
+
+
+
+/*
+// 다른 사람이 짠 코드...
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+int n, k, x, y, s;
+struct Virus {
+	int x;
+	int y;
+	int v;
+};
+vector <Virus> board;
+int lab[201][201]; //비어있는지 확인하기 위해
+
+int dx[4] = { -1, 1, 0, 0 };
+int dy[4] = { 0, 0, -1, 1 };
+
+bool cmp(Virus a, Virus b) {
+	return a.v < b.v;
+}
+
+void spread() {
+	int vLen = board.size();
+	for (int i = 0; i < vLen; i++) {
+		for (int j = 0; j < 4; j++) {
+			int nx = board[i].x + dx[j];
+			int ny = board[i].y + dy[j];
+			if (nx >= 0 && nx < n && ny >= 0 && ny < n && lab[nx][ny] == 0) {
+				lab[nx][ny] = board[i].v;
+				board.push_back({ nx, ny, board[i].v });
+			}
+		}
+	}
+	board.erase(board.begin(), board.begin() + vLen); //이미 다 확인한 것은 다시 확인하지 않아도 됨
+}
+
+int main() {
+	cin >> n >> k;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			int virus;
+			cin >> virus;
+			if(virus>0) board.push_back({ i, j, virus });
+			lab[i][j] = virus;
+		}
+	}
+	cin >> s >> x >> y;
+	sort(board.begin(), board.end(), cmp);
+	for (int i = 0; i < s; i++) spread();
+
+
+	cout << lab[x - 1][y - 1];
+}
+
+
+
+ 
+ */
