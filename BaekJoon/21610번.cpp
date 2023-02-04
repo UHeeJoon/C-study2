87825186751e1eb760bb8aa5b06cedf4a54cea63
YooHeeJoon <yhjyhj3855@gmail.com>
2023-02-04 21:53:44 +0900

    마법사 상어와 비바라기

--- BaekJoon/21610번.cpp
+++ BaekJoon/21610번.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define FAST_IO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
-int moving[9][2] = { {0,0}, {0,-1}, {-1,-1},{-1,0},{-1,1},{0,1},{1, 1},{1,0},{1,-1} };
+constexpr int moving[9][2] = { {0,0}, {0,-1}, {-1,-1},{-1,0},{-1,1},{0,1},{1, 1},{1,0},{1,-1} };
 struct cloud
 {
 	void location_clearance(const int basket_size)
@@ -12,6 +12,14 @@ struct cloud
 	int y;
 	int x;
 };
+
+void clouds_initialize(vector<cloud>& clouds, const int basket_size)
+{
+	clouds.push_back({ basket_size - 1, 0 });
+	clouds.push_back({ basket_size - 1, 1 });
+	clouds.push_back({ basket_size - 2, 0 });
+	clouds.push_back({ basket_size - 2, 1 });
+}
 
 bool check_area(const int basket_size, const int y, const int x)
 {
@@ -29,9 +37,9 @@ void make_clouds(vector<vector<int>>& baskets, vector<cloud>& clouds, vector<vec
 				clouds_location[i][j] = false;
 				continue;
 			}
-			if (baskets[i][j] > 1 )
+			if (baskets[i][j] > 1)
 			{
-				clouds.push_back({i, j});
+				clouds.push_back({ i, j });
 				baskets[i][j] -= 2;
 				clouds_location[i][j] = true;
 			}
@@ -42,7 +50,7 @@ void make_clouds(vector<vector<int>>& baskets, vector<cloud>& clouds, vector<vec
 void water_copy_burg(vector<vector<int>>& baskets, const vector<cloud>& clouds, const int basket_size)
 {
 	vector<vector<int>> copy_baskets(baskets);
-	for(const cloud cloud : clouds)
+	for (const cloud cloud : clouds)
 	{
 		for (int i = 2; i <= 8; i += 2)
 		{
@@ -70,13 +78,21 @@ void move_clouds(vector<vector<int>>& baskets, vector<cloud>& clouds, vector<vec
 	}
 }
 
-void wising_rain(vector<vector<int>>& baskets, vector<cloud>& clouds, const int basket_size, const int move_direction, const int move_squares)
+void wising_rain(vector<vector<int>>& baskets, const int basket_size, int move_command_size)
 {
-	vector<vector<bool>> clouds_location(basket_size, vector<bool>(basket_size));
-	move_clouds(baskets, clouds, clouds_location, basket_size, move_direction, move_squares);
-	water_copy_burg(baskets, clouds, basket_size);
-	vector<cloud>().swap(clouds);
-	make_clouds(baskets, clouds, clouds_location, basket_size);
+	vector<cloud> clouds;
+	clouds_initialize(clouds, basket_size);
+	while (move_command_size--)
+	{
+		vector<vector<bool>> clouds_location(basket_size, vector<bool>(basket_size));
+		int move_direction, move_squares;
+		cin >> move_direction >> move_squares;
+
+		move_clouds(baskets, clouds, clouds_location, basket_size, move_direction, move_squares);
+		water_copy_burg(baskets, clouds, basket_size);
+		vector<cloud>().swap(clouds); // free
+		make_clouds(baskets, clouds, clouds_location, basket_size);
+	}
 }
 
 int print_water_sum(const vector<vector<int>>& baskets, const int basket_size)
@@ -104,17 +120,7 @@ int main() {
 			cin >> baskets[i][j];
 		}
 	}
-	vector<cloud> clouds;
-	clouds.push_back({ basket_size - 1, 0} );
-	clouds.push_back( {basket_size - 1, 1} );
-	clouds.push_back( {basket_size - 2, 0} );
-	clouds.push_back( {basket_size - 2, 1} );
-	while(move_command_size--)
-	{
-		int move_direction, move_squares;
-		cin >> move_direction >> move_squares;
-		wising_rain(baskets, clouds, basket_size, move_direction, move_squares);
-	}
+	wising_rain(baskets, basket_size, move_command_size);
 	cout << print_water_sum(baskets, basket_size) << '\n';
 	return 0;
 }
