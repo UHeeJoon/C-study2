95bb44991d757e4e840e72c685159a7e1c1d223e
YooHeeJoon <yhjyhj3855@gmail.com>
2023-03-17 17:44:46 +0900

    저장

--- BaekJoon/질의응답.cpp
+++ BaekJoon/질의응답.cpp
@@ -1,13 +1,34 @@
 //#pragma warning(disable : 4996)
-// 20165번 인내의 도미노 장인 호석...
-// https://www.acmicpc.net/problem/20165
+// 13702번 이상한 술집...
+// https://www.acmicpc.net/problem/18428
 #include<bits/stdc++.h>
 using namespace std;
 #define FAST_IO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
-
 int main()
 {
 	FAST_IO;
-	int x, n; cin >> x >> n;
+	int r, c; cin >> r >> c;
+	int a, b; cin >> a >> b;
+	for (int i = 0; i < r; i++)
+	{
+		for (int m = 0; m < a; m++)
+		{
+			char chess = 'X';
+			if (i & 1)
+			{
+				chess = '.';
+			}
+			for (int j = 0; j < c; j++)
+			{
+
+				for (int n = 0; n < b; n++)
+				{
+					cout << chess;
+				}
+				chess = chess == '.' ? 'X' : '.';
+			}
+			cout << '\n';
+		}
+	}
 	return 0;
 }
