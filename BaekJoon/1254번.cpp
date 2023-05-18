ce54b321b2071831b3e3c24efab54100eecc3f1e
YooHeeJoon <yhjyhj3855@gmail.com>
2023-05-18 18:54:52 +0900

    팰린드롬 만들기

---
+++ BaekJoon/1254번.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+bool check_pal(const string& str)
+{
+	for (string::size_type i = 0; i < str.length() / 2; i++)
+	{
+		if (str[i] != str[str.length() - 1 - i])
+		{
+			return false;
+		}
+	}
+	return true;
+}
+int main()
+{
+	FAST_IO;
+	string str; cin >> str;
+	int idx = 0;
+	string tmp = str;
+	while (!check_pal(tmp))
+	{
+		tmp = str;
+		for (int i = idx++; i >= 0; i--)
+		{
+			tmp += str[i];
+		}
+	}
+	cout << str.length() + idx << '\n';
+	return 0;
+}
