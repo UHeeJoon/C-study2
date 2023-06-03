584935cb08f1c392a85974bee44a615e13698c1c
YooHeeJoon <yhjyhj3855@gmail.com>
2023-06-03 17:16:31 +0900

    인사성 밝은 곰곰이

---
+++ BaekJoon/25192번.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+int main()
+{
+	FAST_IO;
+	int n; cin >> n;
+	unordered_map<string, int> chat;
+	int ans = 0;
+	while (n--)
+	{
+		string str; cin >> str;
+		if (str == "ENTER")
+		{
+			ans += chat.size() - 1;
+			unordered_map<string, int>().swap(chat);
+		}
+		chat[str]++;
+	}
+	cout << ans + chat.size() << '\n';
+	return 0;
+}
