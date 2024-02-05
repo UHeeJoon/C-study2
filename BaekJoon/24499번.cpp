acd85e06c5039f5f793a0cfb76fbda0d6fadbb89
UHeeJoon <yhjyhj3855@gmail.com>
2024-02-05 18:19:02 +0900

    blobyum

--- BaekJoon/24499번.cpp
+++ BaekJoon/24499번.cpp
@@ -1,5 +1,3 @@
-﻿//#pragma warning(disable : 4996)
-//https://www.acmicpc.net/problem/17175
 #include<bits/stdc++.h>
 using namespace std;
 #define FAST_IO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
