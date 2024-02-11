e66929079ff77caa3572ae0bfbe52eb75e74e2a8
UHeeJoon <yhjyhj3855@gmail.com>
2024-02-11 18:24:02 +0900

    펜윅 트리 추가

--- BaekJoon/1275번.cpp
+++ BaekJoon/1275번.cpp
@@ -35,3 +35,42 @@ int main() {
 	}
 	return 0;
 }
+
+/*
+#include<bits/stdc++.h>
+using namespace std;
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+static void init(vector<int>& v, int idx, int num, int n) {
+	while (idx <= n + 1) {
+		v[idx] += num;
+		idx += (idx & -idx);
+	}
+}
+static int sum(vector<int>& v, int idx) {
+	int _sum = 0;
+	while (idx) {
+		_sum += v[idx];
+		idx -= (idx & -idx);
+	}
+	return _sum;
+}
+int main()
+{
+	FAST_IO;
+	int n, l; cin >> n >> l;
+	vector<int> v(n + 2, 0);
+	for (int i = 1; i <= n; i++) {
+		int num; cin >> num;
+		init(v, i, num, n);
+	}
+	int count = 0;
+	for (int i = 1; i <= n; i++) {
+		int _sum = sum(v, i) - sum(v, (i - l) < 0 ?  0 : i - l);
+		if (129 <= _sum && _sum <= 138 ) {
+			count++;
+		}
+	}
+	cout << count << '\n';
+	return 0;
+}
+*/
