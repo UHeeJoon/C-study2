bea51555ce3245c41677f0e749549460b9fecd78
UHeeJoon <yhjyhj3855@gmail.com>
2024-02-14 16:59:48 +0900

    팩토리얼5

--- BaekJoon/1564번.cpp
+++ BaekJoon/1564번.cpp
@@ -1,11 +1,7 @@
-﻿//#pragma warning(disable : 4996)
-//https://www.acmicpc.net/problem/1517
 #include<bits/stdc++.h>
 using namespace std;
-#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 int main()
 {
-	//FAST_IO;
 	constexpr long long MOD = 1e12;
 	int n; cin >> n;
 	long long mul = 1;
